2d862c2c6d9e958d985e1b55fc412bd63a4289f5
Nicki <nicki@arable.com>
2018-07-21 23:35:43 -0400

    add filter to sort Sources on Works by order entered

--- themes/monza-mod-vyh/functions.php
+++ themes/monza-mod-vyh/functions.php
@@ -194,6 +194,39 @@ function x_get_the_posts_navigation( $args = array() ) {
 }
 
 
+/**
+ * Sort Sources by term_order; this and next function from https://core.trac.wordpress.org/ticket/5857
+ * It's for a very old version of WordPress but is linked from the codex on taxonomies.
+ *
+ * @param array $terms array of objects to be replaced with sorted list
+ * @param integer $id post id
+ * @param string $taxonomy only 'source' is changed.
+ * @return array of objects
+ */
+function plugin_get_the_ordered_terms( $terms, $id, $taxonomy ) {
+    if ( 'source' != $taxonomy ) // only ordering tags for now but could add other taxonomies here.
+        return $terms;
+
+    $terms = wp_cache_get($id, "{$taxonomy}_relationships_sorted");
+    if ( false === $terms ) {
+        $terms = wp_get_object_terms( $id, $taxonomy, array( 'orderby' => 'term_order' ) );
+        wp_cache_add($id, $terms, $taxonomy . '_relationships_sorted');
+    }
+
+    return $terms;
+}
+add_filter( 'get_the_terms', 'plugin_get_the_ordered_terms', 10, 4 );
+
+
+/**
+ * Adds sorting by term_order to source by doing a partial register replacing the default
+ */
+function plugin_register_sorted_sources() {
+    register_taxonomy( 'source', 'works', array( 'sort' => true, 'args' => array( 'orderby' => 'term_order' ) ) );
+}
+add_action( 'init', 'plugin_register_sorted_sources' );
+
+
 require get_template_directory() . '/../monza-mod-vyh/inc/template-tags.php';
 
 ?>
